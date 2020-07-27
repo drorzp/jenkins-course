66c4a507cc0322eac0771d6c081c2369c7acabea
Dror Zeplovitch <drorzp@hotmail.com>
2020-07-27 19:17:55 -0400

    change name

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/drorzp/docker-demo') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Dror Zeplovitch')
+            node / gitConfigEmail('drorzp@hotmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('drorzp/node-example')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
