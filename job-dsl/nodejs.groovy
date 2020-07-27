7dfffadacefcd2429f45b708f923c82e6828160d
Dror Zeplovitch <drorzp@hotmail.com>
2020-07-27 19:11:21 -0400

    change name

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -13,6 +13,7 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
+        shell("echo 'hi there'")
         shell("npm install")
     }
 }
