e149f6b44464c0c49a344916fb9d01ac6bcc4950
Dror Zeplovitch <drorzp@hotmail.com>
2020-07-27 19:34:54 -0400

    change name

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('drorzp/node-example')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('docker')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
